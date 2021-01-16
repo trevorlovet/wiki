4f101b22ced50f227c523d0a1fea63c281e825d8
trevorlovet <trevor.lovet@hotmail.com>
2021-01-16 18:31:39 -0500

    Project

--- wiki project.py
+++ wiki project.py
@@ -1,3 +1,29 @@
-print("hello")
-print("change " + " new")
-print("goodbye")
+import requests
+import pprint
+from bs4 import BeautifulSoup
+
+
+URL = 'https://en.wikipedia.org/wiki/Main_Page'
+
+page = requests.get(URL)
+
+# pprint.pprint(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='mp-tfa')
+
+# print(results.prettify())
+
+linkinfo = results.find_all('a')
+
+mainTitle = (linkinfo[1])
+
+print(mainTitle.string)
+
+for linkinfo in linkinfo:
+    print(linkinfo.string)
+
+
+
+# print(todaysArticle)
